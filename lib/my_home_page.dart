b2442e7b7c87f4b0119fab522b570cdfc068b06c
Said Farhan <Saidfarhan98@gmail.com>
2023-02-28 15:35:52 +0700

    belajar row dan button

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -10,11 +8,45 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My First App"),
-        centerTitle: true,
-        backgroundColor: Colors.amber,
-      ),
-    );
+        appBar: AppBar(
+          title: Text("My First App"),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+        ),
+        body: Column(
+          children: [
+            Flexible(
+              child: Container(
+                // height: double.infinity,
+                height: MediaQuery.of(context).size.width * 0.5,
+                width: MediaQuery.of(context).size.width,
+                color: Color.fromARGB(255, 101, 99, 101),
+                child: const Center(
+                  child: Text(
+                    "Hello World",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                  // color: Colors.white,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Tekan"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                elevation: 2,
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              ),
+            )
+          ],
+        ));
   }
 }
