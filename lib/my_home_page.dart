00966d3646697baa3b87309e28a13942a235de6e
Said Farhan <Saidfarhan98@gmail.com>
2023-02-28 15:13:54 +0700

    mulai belajar flutter

---
+++ lib/my_home_page.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
+}
